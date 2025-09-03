443cd13d5eebda97225783b9edf0e5db7a226739
neverything <neverything@gmail.com>
2025-09-03 16:01:56 +0200

    copying the original topbar over as a start

--- app/Livewire/CustomTopbar.php
+++ app/Livewire/CustomTopbar.php
@@ -2,11 +2,27 @@
 
 namespace App\Livewire;
 
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Livewire\Concerns\HasTenantMenu;
+use Filament\Livewire\Concerns\HasUserMenu;
+use Filament\Schemas\Concerns\InteractsWithSchemas;
+use Filament\Schemas\Contracts\HasSchemas;
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
-class CustomTopbar extends Component
+class CustomTopbar extends Component implements HasActions, HasSchemas
 {
-    public function render()
+    use HasTenantMenu;
+    use HasUserMenu;
+    use InteractsWithActions;
+    use InteractsWithSchemas;
+
+    #[On('refresh-topbar')]
+    public function refresh(): void {}
+
+    public function render(): View
     {
         return view('livewire.custom-topbar');
     }
